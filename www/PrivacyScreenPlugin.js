93d805a2b0d6f70e2a98786973ee307871a1b966
santhosh <santhosh.gdpu@gmail.com>
2018-12-13 20:04:58 +0530

    js modified

--- www/PrivacyScreenPlugin.js
+++ www/PrivacyScreenPlugin.js
@@ -6,3 +6,27 @@
 //exports.activate = function(arg, success, error) {
   //exec(success, error, "PrivacyScreenPlugin", "activate", [arg]);
 //};
+function PrivacyScreenPlugin() {
+}
+
+PrivacyScreenPlugin.prototype.show = function (successCallback, errorCallback) {
+  cordova.exec(
+    successCallback,
+    errorCallback,
+    "PrivacyScreenPlugin",
+    "enable",
+    [{}]
+  );
+};
+
+PrivacyScreenPlugin.prototype.isAvailable = function (successCallback, errorCallback) {
+  cordova.exec(
+    successCallback,
+    errorCallback,
+    "PrivacyScreenPlugin",
+    "disable",
+    [{}]
+  );
+};
+
+module.exports = new PrivacyScreenPlugin();
